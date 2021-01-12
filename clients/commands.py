3b08ae6e21832af5fe3031537874339a1e61e148
Francisco Garcia <garciafran@hotmail.com>
2021-01-12 11:37:32 -0500

    Complete CRUD

--- clients/commands.py
+++ clients/commands.py
@@ -39,21 +39,63 @@ def create(ctx, name, company, email, position):
 @click.pass_context
 def list(ctx):
     """List all clientes"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_list = client_service.list_clients()
+
+    click.echo(' ID  |  NAME  |  COMPANY  | EMAIL  |  POSITION')
+    click.echo('*' * 100)
+
+    for client in client_list:
+        click.echo('{uid} | {name} | {company} | {email} | {position}'.format(
+            uid=client['uid'],
+            name=client['name'],
+            company=client['company'],
+            email=client['email'],
+            position=client['position']
+        ))
 
 
 @clients.command()
+@click.argument('client_uid', type=str)
 @click.pass_context
 def update(ctx, client_uid):
     """Updates a clients"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_list = client_service.list_clients()
+
+    client = [client for client in client_list if client['uid'] == client_uid]
+
+    if client:
+        client = _update_client_flow(Client(**client[0]))
+        client_service.update_client(client)
+
+        click.echo('Client update')
+    else:
+        click.echo('Client not found')
+
+
+def _update_client_flow(client):
+    click.echo('Leave empty if you dont want to modify the value')
+
+    client.name = click.prompt('New name', type=str, default=client.name)
+    client.company = click.prompt('New company', type=str, default=client.company)
+    client.email = click.prompt('New email', type=str, default=client.email)
+    client.position = click.prompt('New position', type=str, default=client.position)
+
+    return client
 
 
 @clients.command()
+@click.argument('client_uid', type=str)
 @click.pass_context
 def delete(ctx, client_uid):
     """Delete a client"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    if click.confirm('Are you sure want to delete the client with uid: {}'.format(client_uid)):
+        client_service.delete_client(client_uid)
 
 
 all = clients
