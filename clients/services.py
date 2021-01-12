9fd384a26991959257d4c54a7ed1beab60f623ee
Francisco Garcia <garciafran@hotmail.com>
2021-01-12 11:46:28 -0500

    update services.py

--- clients/services.py
+++ clients/services.py
@@ -8,7 +8,7 @@ class ClientService:
     def __init__(self, table_name):
         self.table_name = table_name
 
-    
+
     def create_client(self, client):
         with open(self.table_name, mode='a') as f:
             writer = csv.DictWriter(f, fieldnames=Client.schema())
